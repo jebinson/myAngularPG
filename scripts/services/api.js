fbe608e132e6ce05c0b14c71ddad482e9ea87e45
jebinson <jebinson@gmail.com>
2016-11-22 13:16:40 +0800

    json upload

---
+++ scripts/services/api.js
@@ -0,0 +1,49 @@
+angular.module('myAngApp')
+.factory('Api', ['$http',
+function ($http) {
+    // List all interface / public end points that can be consumed
+    var factory = {
+        Get: Get,
+        Post: Post
+    };
+
+    //get data for api's
+    function Get(apiActionName) {
+
+        return $http.get(apiActionName)
+            .then(function (response) {
+                if (response.status == 200) {
+                    return response.data;
+                }
+                alert('Error getting data from Api - ' + response.data.message);
+            },
+            function (error) {
+                //report thr error
+                alert('Error getting data from Api - ' + apiActionName);
+            },
+            function (progress) {
+                //report the progress
+            })
+    }
+
+    //post data for api's
+    function Post(apiActionName, apiData) {
+
+        return $http.post(apiActionName, apiData)
+            .then(function (response) {
+                if (response.status == 200) {
+                    return response.data;
+                }
+                alert('Error getting data from Api - ' + response.data.message);
+            },
+            function (error) {
+                //report thr error
+                alert('Error getting data from Api - ' + apiActionName);
+            },
+            function (progress) {
+                //report the progress
+            })
+    }
+
+    return factory;
+}]);
