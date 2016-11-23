6b6327c453ddb84fd67bf2c97b7ab53cb1bdbc55
jebinson <jebinson@gmail.com>
2016-11-23 17:54:45 +0800

    Wednesday!

--- scripts/app.js
+++ scripts/app.js
@@ -8,28 +8,37 @@
 		var viewBase = '/templates/';
 
 		$routeProvider
-			.when('/', {redirectTo: '/home'})
+		.when('/', {redirectTo: '/home'})
 
-			.when('/home', {
-				controller : 'homeController',
-				templateUrl: viewBase + 'home.html',
-				controllerAs: 'vm',
-				title: 'My Angular Play Ground'
-			})
-			.when('/hotelSearch', {
-				controller : 'hotelController',
-				templateUrl: viewBase + 'hotelSearch.html',
-				controllerAs: 'vm',
-				title: 'Hotel Search'
-			})
-			.when('/motorSavings', {
-				controller : 'motorSavings',
-				templateUrl: viewBase + 'motorSavings.html',
-				controllerAs: 'vm',
-				title: 'Motor Savings Calculator'
-			})
-			.otherwise({redirectTo: '/'});
-	}])
+		.when('/home', {
+			controller : 'homeController',
+			templateUrl: viewBase + 'home.html',
+			controllerAs: 'vm',
+			title: 'My Angular Play Ground'
+		})
+		.when('/hotelSearch', {
+			controller : 'hotelController',
+			templateUrl: viewBase + 'hotelSearch.html',
+			controllerAs: 'vm',
+			title: 'Hotel Search'
+		})
+		.when('/motorSavings', {
+			controller : 'motorSavings',
+			templateUrl: viewBase + 'motorSavings.html',
+			controllerAs: 'vm',
+			title: 'Motor Savings Calculator'
+		})
+		.otherwise({redirectTo: '/'});
+	}]);
 
+	app.run(['$location', '$rootScope', function ($location, $rootScope) {
+		$rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+
+			if (current.hasOwnProperty('$$route')) {
+
+				$rootScope.title = current.$$route.title;
+			}
+		});
+	}]);
 
 })();
--- scripts/controllers/hotelController.js
+++ scripts/controllers/hotelController.js
@@ -4,6 +4,7 @@
 	.controller('hotelController', ['$scope', "Api",function($scope, Api){
 
 		var vm = this;
+		vm.title = "Hotel Search";
 		vm.data = [];
 
 		var url = 'https://raw.githubusercontent.com/jebinson/myAngularPG/master/data/hotelData.json';
@@ -11,6 +12,12 @@
 		Api.Get(url).then(function(response){
 			vm.data = response;
 		})
+
+		vm.totalDisplayed = 5;
+
+		vm.loadMore = function () {
+			vm.totalDisplayed += 5;
+		}
 
 	}]);
 	
